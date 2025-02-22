31148fb634c8233af1bf2ed9b0bd7f17430de7bc
Cartaxo <carlos.pijanowski@gmail.com>
2025-02-22 16:17:39 -0300

    inicial commit

---
+++ src/main/java/br/com/seteideias/terraform202502231600/Terraform202502231600Application.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.terraform202502231600;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Terraform202502231600Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Terraform202502231600Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/seteideias/terraform202502231600/Terraform202502231600ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.terraform202502231600;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Terraform202502231600ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
