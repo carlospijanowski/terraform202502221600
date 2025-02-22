19b5332ee18c836355c555b59a2ed0f55559e883
Cartaxo <carlos.pijanowski@gmail.com>
2025-02-22 17:13:54 -0300

    atualizei ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "app_java" {
   ami           = "ami-05b10e08d247fb927"  # Amazon Linux 2
   instance_type = "t2.micro"
-  key_name      = "minha-chave-aws"  # Use uma chave SSH válida
+  key_name      = "cartaxopair"  # Use uma chave SSH válida
 
   user_data = <<-EOF
               #!/bin/bash
