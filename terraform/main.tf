5a58c28bec7a02900a632951cb118698231d0236
Cartaxo <carlos.pijanowski@gmail.com>
2025-02-22 16:23:55 -0300

    added configs. action and terraform config

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_java" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
+  instance_type = "t2.micro"
+  key_name      = "minha-chave-aws"  # Use uma chave SSH válida
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras enable corretto8
+              sudo yum install -y java-1.8.0-amazon-corretto
+              cd /home/ec2-user
+              wget https://meus-arquivos/meu-app.jar
+              nohup java -jar meu-app.jar > output.log 2>&1 &
+              EOF
+
+  tags = {
+    Name = "Servidor-Java"
+  }
+}
+
+output "public_ip" {
+  description = "IP público da EC2"
+  value       = aws_instance.app_java.public_ip
+}
